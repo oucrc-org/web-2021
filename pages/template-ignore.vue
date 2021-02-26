f1b832e462295d3380d2d23438e82a3284231562
AAAR Salmon <TenzikuJohn@gmail.com>
2021-02-26 22:53:14 +0900

    [refactor] テンプレートファイル類を整理

---
+++ pages/template-ignore.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		<Header />
+		<div id="containts">
+			<!-- ここにコンテンツ -->
+		</div>
+		<Footer />
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+
+</style>
