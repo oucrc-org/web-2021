edc40e55621abd537e9719127d16e5ef8d31e1c7
AAAR Salmon <TenzikuJohn@gmail.com>
2021-02-27 02:49:42 +0900

    [add] ヘッダー作成 #2

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,19 @@
 <template>
-	<div id="header" class="text-lg flex-row">
-		<div>ロゴ</div>
-		<nav>ナビゲーション</nav>
-		<img src="" alt="検索">
-		<img src="" alt="メニュー" class="md:hidden">
+	<div id="header" class="text-lg flex flex-row justify-between">
+		<NuxtLink to="/" class="m-2 flex flex-row">
+			<img src="~/assets/images/oucrc-logo.svg" alt="ロゴ">
+			<img src="~/assets/images/oucrc-label.svg" alt="岡山大学電子計算機研究会" class="hidden md:block">
+		</NuxtLink>
+		<div class="m-2 hidden md:flex flex-grow justify-end">
+			<nav id="navigation" class="self-center">
+				<NuxtLink to="/">ホーム</NuxtLink>
+				<NuxtLink to="/articles">作品紹介</NuxtLink>
+				<NuxtLink to="/members">部員紹介</NuxtLink>
+				<NuxtLink to="/join">入部フォーム</NuxtLink>
+			</nav>
+			<img src="~/assets/images/search.svg" width="23" height="23" alt="検索" class="m-2">
+		</div>
+		<img src="~/assets/images/hamburger.svg" width="23" height="16" alt="メニュー" class="m-2">
 	</div>
 </template>
 
@@ -14,5 +24,7 @@ export default {
 </script>
 
 <style scoped>
-
+#navigation > * {
+	@apply m-1
+}
 </style>
