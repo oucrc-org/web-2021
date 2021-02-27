aff077a7bcdcd0824d7f9aa28ea399c79704c72e
smpny7 <symphonymode7@icloud.com>
2021-02-27 23:07:42 +0900

    [add] ランディングページ 電子計算機研究会とは

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,32 +34,11 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.double-line-button {
+  @apply border-double border-4 border-secondary font-semibold inline-block py-2 relative text-secondary w-48
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.double-line-button > img {
+  @apply absolute bottom-0 h-4 mr-2 my-auto right-0 top-0
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#262626',
+        'secondary': '#595959',
+        'divider': '#DADADA'
+      },
+      scale: {
+        '102': '1.02'
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem'
+      }
+    },
   },
   variants: {},
   plugins: [],
