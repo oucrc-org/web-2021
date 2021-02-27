b6ee2da29add5338935d92131d5dd3dfa6bb2c87
smpny7 <symphonymode7@icloud.com>
2021-02-27 16:39:55 +0900

    [add] ランディングページ 電算研の部室

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-	<div id="footer">
+	<div id="footer" class="bg-white text-center relative z-50">
 		<div class="text-sm">Copyright &copy; 2021 OUCRC</div>
 	</div>
 </template>
 
 <script>
 export default {
-	
+
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,89 @@
 <template>
   <div id="app">
-    <div id="contents">
-      <code><pre>{{ json }}</pre></code>
-    </div>
+    <div class="parallax bg-no-repeat bg-center bg-cover h-full fixed left-0 top-0 w-full z-0"></div>
+
+    <!-- ▼ ヒーローエリア -->
+    <section class="bg-white h-64 relative">
+      <br><br><br>スライドショー
+    </section>
+    <!-- ▲ ヒーローエリア -->
+
+
+    <!-- ▼ 電子計算機研究会とは -->
+    <section class="bg-red-300 h-64 relative w-full">
+      <div class="container mx-auto">
+        電子計算機研究会とは
+      </div>
+    </section>
+    <!-- ▲ 電子計算機研究会とは -->
+
+
+    <!-- ▼ 電算研の部室 -->
+    <section class="relative w-full z-10" style="height: 76vh">
+      <div class="container h-full relative mx-auto">
+        <img class="oucrc-room-label absolute left-0" src="~/assets/images/oucrc-room-label.png" alt="電算研の部室">
+        <a href="#">
+          <img class="oucrc-room-button absolute right-0 transform hover:scale-105 transition duration-300 ease-in-out"
+               src="~/assets/images/oucrc-room-button.png" alt="部室に何があるの？">
+        </a>
+      </div>
+    </section>
+    <!-- ▲ 電算研の部室 -->
+
+
+    <!-- ▼ お知らせ -->
+    <section class="bg-red-300 h-64 relative">
+      お知らせ
+    </section>
+    <!-- ▲ お知らせ -->
+
+
+    <!-- ▼ お問い合わせ -->
+    <section class="bg-white h-64 relative">
+      お問い合わせ
+    </section>
+    <!-- ▲ お問い合わせ -->
+
+
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      json: 'No data'
-    }
-  },
-  mounted() {
-    // APIはこれで叩けます！！
-    axios
-      .get('https://oucrc.microcms.io/api/v1/article', {
-        headers: {
-        'X-API-KEY': '6d1b79a2-58de-49aa-bb5c-d2828e0d7d47'
-        }
-      })
-      .then(response => {
-        this.json = response.data
-      })
-  }
-};
+// import axios from 'axios'
+//
+// export default {
+//   data() {
+//     return {
+//       json: 'No data'
+//     }
+//   },
+//   mounted() {
+//     // APIはこれで叩けます！！
+//     axios
+//       .get('https://oucrc.microcms.io/api/v1/article', {
+//         headers: {
+//         'X-API-KEY': '6d1b79a2-58de-49aa-bb5c-d2828e0d7d47'
+//         }
+//       })
+//       .then(response => {
+//         this.json = response.data
+//       })
+//   }
+// };
 </script>
 
 <style scoped>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+.oucrc-room-label {
+  height: 40vh;
+  top: 5vh;
 }
-*/
+
+.oucrc-room-button {
+  bottom: 5vh;
+  height: 7vh;
+}
+
+.parallax {
+  background-image: url(~/assets/images/oucrc-room.png)
+}
 </style>
