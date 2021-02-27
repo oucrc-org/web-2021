42e24bd14647d4012174b05327d0a051f8780e43
AAAR Salmon <TenzikuJohn@gmail.com>
2021-02-27 16:47:24 +0900

    [feature] Buttonコンポーネントを作成

---
+++ components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="button">
+		<span>{{ label }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['label']
+}
+</script>
+
+<style scoped>
+.button {
+	position: relative;
+	border: 4px double black;
+	text-align: center;
+	transition-duration: 0.3s;
+}
+
+.button > span::after {
+	position: absolute;
+	content: '>';
+	right: 0.25rem;
+}
+
+.button:hover {
+	transition-duration: 0.3s;
+	background-color: #eee;
+}
+</style>
