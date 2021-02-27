fecabd647d2ac93c99a8bdda9fa2978025ce139e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-02-28 00:07:08 +0900

    [fix] MathJax3の削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,6 @@ export default {
     ],
     script: [
       { src: 'https://polyfill.io/v3/polyfill.min.js?features=es6', mode: 'client' },
-      { id: 'MathJax-script', async: true, src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js', mode: 'client' },
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_HTML' }
     ]
   },
