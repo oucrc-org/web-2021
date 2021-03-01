81ce9ae26447cd3e47644ea8d0373c8440f05e6e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-03-01 23:12:32 +0900

    [add] 通信機能を追加

--- pages/members/index.vue
+++ pages/members/index.vue
@@ -2,33 +2,73 @@
   <div class="container mb-32 mt-16 mx-auto px-10">
     <Title label="部員一覧"/>
 
-    <div v-for="n in 4">
+    <div v-for="year in years">
       <div class="mb-12 mt-20 relative text-center">
         <div class="border-b border-heading pt-3"></div>
         <h3
-          class="absolute bg-white font-bold inline-block mx-auto left-0 right-0 text-subtext top-0 tracking-widest w-24">
-          １回生
+          class="absolute bg-white font-bold inline-block mx-auto left-0 right-0 text-subtext top-0 tracking-widest w-40">
+          {{year}}年度入部
         </h3>
       </div>
 
-      <div class="grid grid-cols-5 gap-10 px-32">
-        <NuxtLink to="members/{member_id}" class="transform hover:scale-103 transition duration-500 ease-in-out"
-                  v-for="n in 9">
-          <div class="px-5">
-            <img class="rounded-full" alt="仮"
-                 src="https://avatars.githubusercontent.com/u/58544849?s=460&u=9e921c5bba0462a1873f73f3d8d98dcad33a2bd7&v=4">
-          </div>
-          <p class="font-bold mt-3 text-center text-lg text-secondary">いけちぃ</p>
-          <div class="overflow-hidden px-3 text-center text-sm text-subtext">
-            <p class="overflow-hidden overflow- whitespace-no-wrap" style="text-overflow: ellipsis">Apple信徒</p>
-          </div>
-        </NuxtLink>
+
+
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6 lg:gap-10 px-4 sm:px-12 xl:px-32">
+        <div v-for="content in members.contents" v-if="content.enteryear === year">
+          <MemberIndexCard
+            :href="'member/' + content.id"
+            :img-path="content.avatar !== void(0) ? content.avatar.url : null"
+            :name="content.name"
+            :status="content.status"
+          />
+        </div>
       </div>
     </div>
 
   </div>
 </template>
 
+
 <script>
-export default {}
+import axios from "axios";
+
+export default {
+  data(){
+    return{
+      members: {
+        contents: []
+      },
+      years: []
+    };
+  },
+  mounted() {
+    this.setYear(this.members.contents)
+  },
+  methods: {
+    setYear(contents){
+      let yearSet = new Set()
+      for(let content of contents){
+        yearSet.add(content.enteryear)
+      }
+      this.years = yearSet
+    }
+  },
+  asyncData({ error }) {
+    return axios.get('https://oucrc.microcms.io/api/v1/member', {
+      headers: {
+        'X-API-KEY': '6d1b79a2-58de-49aa-bb5c-d2828e0d7d47'
+      }
+    }).then(response => {
+      console.log(response.data)
+      return {
+        members: response.data
+      }
+    }).catch(e => {
+      error({
+        statusCode: e.response.status,
+        message: e.message
+      })
+    })
+  }
+};
 </script>
