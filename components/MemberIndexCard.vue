5aafca6746942a87cad5fb80695634a203eeba59
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-03-01 23:12:32 +0900

    [add] 通信機能を追加

---
+++ components/MemberIndexCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <NuxtLink :to="href" class="transform hover:scale-103 transition duration-500 ease-in-out">
+    <div class="px-5">
+      <img
+        v-if="imgPath !== null"
+        class="rounded-full w-full h-full object-cover" alt="画像"
+        :src="imgPath">
+      <img v-else class="rounded-full　object-cover h-full w-full" src="@/assets/images/dummy.png" alt="">
+    </div>
+    <p class="font-bold mt-3 text-center text-lg text-secondary">{{ name }}</p>
+    <div class="overflow-hidden px-3 text-center text-sm text-subtext">
+      <p class="overflow-hidden overflow- whitespace-no-wrap" style="text-overflow: ellipsis">{{ status }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'MemberIndexCard',
+  props: {
+    href: {
+      type: String,
+      default: '#'
+    },
+    imgPath: {
+      type: String,
+      default: null
+    },
+    name: {
+      type: String,
+      default: null
+    },
+    status: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
