11c2ed1e285abde81af7ec44708701aa3cc90524
smpny7 <symphonymode7@icloud.com>
2021-03-02 17:58:53 +0900

    [add] 入部フォーム 仮画面

---
+++ pages/join/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container text-center mb-32 mx-auto">
+  <img class="mt-32 mx-auto w-64" src="@/assets/images/oucrc-logo.png" alt="Logo">
+  <p class="mt-10 text-3xl">入部申し込みは、2021年4月１日より開始します！</p>
+  <NuxtLink to="/">
+    <DoubleLineButton label="ホーム" class="mt-16"/>
+  </NuxtLink>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
