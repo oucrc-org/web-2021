19bd9b8eff803cc9c9f6454853eb1b472606a70b
smpny7 <symphonymode7@icloud.com>
2021-03-06 13:00:54 +0900

    [add] カテゴリー&シリーズ クリック時に検索

--- components/LabeledCheckbox.vue
+++ components/LabeledCheckbox.vue
@@ -1,10 +1,10 @@
 <template>
-  <label :for="key" class="font-semibold inline-block text-sm mx-1 my-1 py-2 pl-4 pr-5 rounded-md"
+  <label :for="key" class="font-semibold inline-block text-sm mr-2 my-1 py-2 pl-3 pr-5 rounded-md"
          :class="[checked ? 'bg-secondary text-highlight' : 'bg-blockquote text-secondary']">
     <img class="inline h-6 mr-2"
          :src="name === 'category' ? ( checked ? require('@/assets/images/category-checked.svg') : require('@/assets/images/category.svg') ) : ( checked ? require('@/assets/images/series-checked.svg') : require('@/assets/images/series.svg') )"
          alt="Image">
-    <input :id="key" type="checkbox" v-model="checked" class="hidden" :value="value" :name="name">
+    <input :id="key" type="checkbox" v-model="checked" class="hidden" :value="value" :name="name" @click="runSearch">
     {{ label }}
   </label>
 </template>
@@ -29,6 +29,11 @@ export default {
     value: {
       type: String,
       default: ''
+    }
+  },
+  methods: {
+    runSearch(){
+      this.$emit('search');
     }
   }
 }
