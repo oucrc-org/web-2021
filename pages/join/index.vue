e40e411c2d81566340a1a7fb6bf9b26311ce7003
smpny7 <symphonymode7@icloud.com>
2021-03-08 21:30:51 +0900

    [fix] 入部申し込み スマホ デザイン調整

--- pages/join/index.vue
+++ pages/join/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container text-center mb-32 mx-auto">
-  <img class="mt-32 mx-auto w-64" src="@/assets/images/oucrc-logo.png" alt="Logo">
-  <p class="mt-10 text-3xl">入部申し込みは、2021年4月1日より開始します！</p>
+  <img class="mt-16 sm:mt-32 mx-auto w-48 sm:w-64" src="@/assets/images/oucrc-logo.png" alt="Logo">
+  <p class="mt-10 px-3 text-xl sm:text-3xl tracking-widest">入部申し込みは、<br class="sm:hidden">2021年4月1日より開始します！</p>
   <NuxtLink to="/">
     <DoubleLineButton label="ホーム" class="mt-16"/>
   </NuxtLink>
