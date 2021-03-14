ba1bffcd138d6ebc6a637737040afe7acaea78ad
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-03-14 20:34:19 +0900

    [fix] 環境変数を設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,5 +81,10 @@ export default {
     configPath: 'tailwind.config.js',
     exposeConfig: false,
     config: {}
+  },
+
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+    X_API_KEY: process.env.X_API_KEY
   }
 }
