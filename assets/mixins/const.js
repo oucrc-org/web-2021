2fe55ca16298a0735fb44035fd92413a77db4510
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-04-25 20:16:10 +0900

    [fix] ランディングのOGPに画像をセット

--- assets/mixins/const.js
+++ assets/mixins/const.js
@@ -3,5 +3,5 @@ export default Object.freeze({
   TYPE: 'website',
   DESCRIPTION: '岡山大学電子計算機研究会は、パソコンを使ってプログラミングやDTMをしたり、電子工作や3Dプリンターなど "モノづくり" をしている部活です！部にはVRゴーグルやタブレットなどの機材があり、デバッグなども気軽にできます！また、電子工作のパーツや3Dプリンターなどもあるため、ハードルなく "モノづくり" の世界に入ることができます！そして、息抜きにはゲーム大会を開いたり、漫画を読むこともできます。岡大に入学して部活動・サークル選びに迷っている方は、ぜひ一度遊びに来てください！',
   URL: '',
-  IMAGE: '@/assets/images/cover.png'
+  IMAGE: 'https://images.microcms-assets.io/assets/9db8326938b34b1381d6805cc5e10b04/2d972c34d0a5453cb97dace235ac1fc0/oucrc-logo.png'
 })
