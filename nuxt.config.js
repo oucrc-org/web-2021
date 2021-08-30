aad0f5aeae3ea50a091ae04cbf8532a5825a39ad
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-08-30 16:34:26 +0900

    [fix] スクロールバーの非表示

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/index.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
