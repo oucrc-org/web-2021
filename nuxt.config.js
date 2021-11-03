2e1afd6e61929aaacf962847c5631b384970db62
AAAR-Salmon <tenzikujohn@gmail.com>
2021-11-03 21:29:47 +0900

    Fix: WindowsのChromeでスクロールバーが表示され続けていた問題を修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/index.css',
+    '@/assets/css/index.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
