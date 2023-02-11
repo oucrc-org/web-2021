ce3b3098c25d309931533782fdb72c6131fdd176
temasaguru <temasaguru@gmail.com>
2023-02-12 05:52:15 +0900

    refactor: fetch順番を修正

--- server/api/member/[id].get.ts
+++ server/api/member/[id].get.ts
@@ -3,10 +3,18 @@ import client from '~/server/micro-cms'
 export default defineEventHandler(async (event) => {
   const id = event.context.params?.id
   if (typeof id === 'string') {
-    return await client.get({
+    const member = await client.get({
       endpoint: 'member',
       contentId: id,
     })
+    const articles = await client.getList({
+      endpoint: 'article',
+      queries: {
+        limit: 12,
+        filters: `name[equals]${id}`,
+      },
+    })
+    return { member, articles }
   } else {
     return null
   }
